ca56f787ea78636fc8ee9decf1fdbf6473cad540
Claudio-Emmolo <claudioemmolo90@gmail.com>
2022-11-28 16:27:14 +0100

    Snack-1 Add Do-While

--- js/Snack-1.js
+++ js/Snack-1.js
@@ -1,10 +1,10 @@
 // Continuare a chiedere all'utente un numero tra 1 e 10 fino a quando l'utente non inserisce il valore corretto
 
-let userNumber = parseInt(prompt ("Inserisci un numero tra 1 e 10"));
-
-while ( (userNumber < 1) || (userNumber > 10) ){
+let userNumber;
+do{
     userNumber = parseInt(prompt ("Inserisci un numero tra 1 e 10"));
 }
+while ( (userNumber < 1) || (userNumber > 10) )
 
 console.log("Hai inserito" + " " + userNumber );
 
