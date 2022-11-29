2c65c54facc704f3aa32f5244973ac12fe74cc17
Claudio-Emmolo <claudioemmolo90@gmail.com>
2022-11-29 12:46:46 +0100

    Snack-6 Add If

--- js/Snack-6.js
+++ js/Snack-6.js
@@ -1,12 +1,16 @@
 //Crea una funzione che dato un array, calcoli la somma dei suoi elementi.
 
-const numberList = [10, 2, 2, 2, "cioao"];
+const numberList = [10, 2, 2, 2, "80",  "ciao"];
 
 function sum(array){
     let calc = 0;
 
     for (let i = 0 ; i < array.length ; i++){
-            calc += array[i];
+        const castedElement = parseInt(array[i], 10);
+
+        if ( !Number.isNaN(castedElement)){
+            calc += castedElement;
+        }
     }
     return calc;
 }
